7d5795c0c10f634639ea4c2ad2d75268f7b21c5a
davidtoca <david.virusfel@gmail.com>
2013-04-16 23:38:30 -0500

    Chapter 12 -- Asking the right questions.
    Our hero is been ask about it's skills, and also WTF there isn't any
    chapters between 6 and 11? !

---
+++ ex12.py
@@ -0,0 +1,16 @@
+#our hero remains in the temple and is been asking for his training so far
+print "How are your skills with the math operators?"
+math_operations_skills = raw_input("skills: ")
+print "And what about your commenting ones?"
+commenting_skills = raw_input("skills: ")
+print "Why there isn't any more chapters between 6 and 11?"
+why_missing_chapters = raw_input("whyyy: ")
+print
+print "Ohhh ok, now I get it, it was because %s, mistery solved !" % why_missing_chapters
+
+print "Now back to your skills, your current status is:"
+print "*" * 10
+print "math operations =", math_operations_skills
+print "*" * 10
+print "commenting =", commenting_skills
+print "*" * 10
