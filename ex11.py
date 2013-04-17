01ddb4df4e78db70134700f2e8880dcc1bd8f436
davidtoca <david.virusfel@gmail.com>
2013-04-16 23:12:23 -0500

    Chapter 11 ---  Fall down seven times, stand up eight.
    Our apprendice meet a wise master with a listening problem, he ask him
    several question, and after some misunderstanding, he was able to get
    his help in his path.

---
+++ ex11.py
@@ -0,0 +1,11 @@
+#our aprendice enters into a old temple, there he meet with the great master matsumoto
+print "How dares to bother me?"
+my_name = raw_input()
+print "%s what bring you here?" % my_name
+my_propouse = raw_input()
+print "Do you bother just for that triviality?"
+my_response = raw_input()
+print "Say no more ! and get out of here !!" #he doesn't even hear his response
+raw_input() # some suspense
+print "Oh sorry, I didn't here you the first time, so you're learning python and yo came here for %s" % my_propouse
+print "very well, I'll do the best I can for helping you %s-san" %my_name
