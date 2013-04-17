d47f7f232af83cad286e24eb14ff5307aaa5e837
davidtoca <david.virusfel@gmail.com>
2013-04-16 22:09:22 -0500

    Chapter 4 --- vessels full of data.
    In this big band theory episode, penny(aka grasshopper), learn the
    Schrödinger's cat experiment and how to use variables in the way.

---
+++ ex4.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+physicist = "Schrödinger"
+
+cat = "cat" #go figured !
+
+experiment_explanation = physicist, "'s ",cat," is a thought experiment, sometimes described as a paradox, devised by Austrian physicist Erwin Schrödinger in 1935. It illustrates what he saw as the problem of the Copenhagen interpretation of quantum mechanics applied to everyday objects, resulting in a contradiction with common sense. The scenario presents a cat that may be both alive and dead, depending on an earlier random event. Although the original \"experiment\" was imaginary, similar principles have been researched and used in practical applications. The thought experiment is also often featured in theoretical discussions of the interpretations of quantum mechanics. In the course of developing this experiment, Schrödinger coined the term Verschränkung (entanglement)."
+
+steel_chamber = cat
+
+exercies_explanation = "I just wanna show you how to use variables, F%%$^ me rigth?"
+
+print experiment_explanation
+print "" #\n? whats that? !
+
+print "so, let me see.."
+print "what's inside the steel_chamber? :", steel_chamber
+print "who is the physicist? is: ",physicist
+print "what is the most adorable thing ever? ", cat
+
+print "ohh ok.., and this is important because..."
+print exercies_explanation
+
+
+
+
+
