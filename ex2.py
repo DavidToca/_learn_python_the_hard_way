8b087155e3aaf27aa1c4506142ab70928ac123ad
davidtoca <david.virusfel@gmail.com>
2013-04-16 21:05:36 -0500

    Chapter 2, Listen to the silent voice of the comments.
    The grasshopper meets with the ancient spirit of the comments, grown with
    the souls of thousands programmers, he teach it the wise words of samuel L. jackson.
    \#PROGRAMMING MOTHEFUCKER !

---
+++ ex2.py
@@ -0,0 +1,22 @@
+print "hello? who's there?"
+# hello, this is the spirit of the COMMENTS that is now taking to you through your mind
+print "ohh I see, so, only I can hear you?"
+#That's rigth !
+print "LOL, I must look like an idiot for someone who's not listen to the ANCIENT GOD OF COMMNETS "
+# XD
+#indeed
+#ok
+#lets
+#begin
+#with
+print "Wait wait wait, can you talk more fluently?"
+"""
+
+Sure, sure , I'm sorry , long time I didn't do this, now I will tell you the story
+of the lorem ipsum according to SAMUEL L. JACKSON...
+
+The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee.
+
+"""
+
+
