1677fb995b69e1ed7af1ff76fb2f3dc8bf32605c
davidtoca <david.virusfel@gmail.com>
2013-04-16 21:35:27 -0500

    Chapter 3 --- Using the power of operators.
    Our Hero is now facing a hard test, he has to use the power of operators
    to answer a series of questions that no human can solve for his own
    (come one, even a a primary school kid can solve this).

---
+++ ex3.py
@@ -0,0 +1,15 @@
+print "Did you know more than a primary school kid?"
+print "First question, one plus one is.."
+print 2
+print "Ok that was easy, now, 2 less 2 is.."
+print 0
+print "Now a little bit harder, 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 is.."
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print "I bet you count with your fingers !"
+print "next one, is 3 + 2 -8 % 3 + 7 - 9 * 4 / 2< 5 - 7 ?"
+print  3 + 2 -8 % 3 + 7 - 9 * 4 / 2< 5 - 7 
+print "nOOO, wrooon... oh wait, yeah, your rigth"
+print "and 5/2 is.."
+print  5/2
+print "wroooong !!!, 5/2 is ", 5.0/2
+print "basic algebra hahaha, check mate"
