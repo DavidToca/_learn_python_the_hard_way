64c5e0b0998081e86d0723c8bf46f7dcd5d4efab
davidtoca <david.virusfel@gmail.com>
2013-04-16 22:24:36 -0500

    Chapter 5 --- If you know others and know yourself, you will not be imperiled in a hundred battles.
    In this episode, our favorite pokemon trainer use his pokedex to identify a new pokemon with
    a very grumpy face.

---
+++ ex5.py
@@ -0,0 +1,16 @@
+my_name = "Tardar Sauce"
+my_nickname = "Mr. grumpy"
+my_age = 99
+my_mood = "grumpy"
+my_hair = "pretty"
+
+quote = "I had fun onces...it was horrible"
+
+print "Hello I'm %s, but everyone knows me as " % my_name, my_nickname
+print "Maybe you remember me for sites like reddit and events like SXSW"
+print "Im %d years old (or at least that's what I feel)" % my_age
+
+print "if you could see my face, you'll know my mood always is %s" % my_mood
+
+print "a quote that defines me: %s" % quote
+
